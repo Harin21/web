950caf39805937942f9b2f004fed0127c9de04ef
Hariniranjan <23mca18@mgits.ac.in>
2023-11-07 16:00:03 +0530

    c02

---
+++ myscript.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script>
+funtion myfunction(){
+document.getElementById("demo").innerHTML="paragraph changed";
+}
+</script>
+</head><body>
+<h3>Demo Javascript</h3>
+<p id="demo">A paragraph </p>
+<button type="button" onclick="my function()">Try it</button>
+</body>
+</html>
